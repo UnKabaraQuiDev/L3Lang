71aab0e6095e9ca6b98cae7deaa4840073a94871
UnKabaraQuiDev <unkabaraquidev@github.com>
2024-06-12 19:30:11 +0200

    Set CompilerOptions.THROW = true

--- src/main/java/lu/pcy113/l3/utils/CompilerOptions.java
+++ src/main/java/lu/pcy113/l3/utils/CompilerOptions.java
@@ -2,7 +2,7 @@ package lu.pcy113.l3.utils;
 
 public final class CompilerOptions {
 
-	public static boolean THROW_UNREACHABLE_CODE = false;
-	public static boolean THROW_RETURN_SAFETY = false;
+	public static boolean THROW_UNREACHABLE_CODE = true;
+	public static boolean THROW_RETURN_SAFETY = true;
 
 }
